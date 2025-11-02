c0923665d7e90c23d1879ce108e4022fc2fedffa
Yusufhan Keleş <yusudhan@gmail.com>
2025-11-02 22:53:08 +0300

    Fix

--- src/app/sayfalar/dergi-sayi/page.tsx
+++ src/app/sayfalar/dergi-sayi/page.tsx
@@ -1,16 +1,5 @@
-// src/app/sayfalar/dergi-detay/page.tsx
-"use client";
+import SearchParamsWrapper from "./SearchParamsSayiWrapper";
 
-import { Suspense } from "react";
-import Inner from "./_inner";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function Page() {
-    return (
-        <Suspense fallback={null}>
-            <Inner />
-        </Suspense>
-    );
+export default function DergiDetayPage() {
+    return <SearchParamsWrapper />;
 }
